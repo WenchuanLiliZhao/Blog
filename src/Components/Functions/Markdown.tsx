ee7d5cd7f9e476bec32427d0eeb3bd372d020ce6
Wenchuan <111623150+WenchuanLiliZhao@users.noreply.github.com>
2024-10-25 14:45:57 +0800

    画

--- src/Components/Functions/Markdown.tsx
+++ src/Components/Functions/Markdown.tsx
@@ -1,24 +1,25 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import 'katex/dist/katex.min.css';
 import remarkGfm from 'remark-gfm';
+import rehypeHighlight from 'rehype-highlight';
+import 'highlight.js/styles/github.css'; // Use a highlight.js theme of your choice
 
 interface Props {
-  children: any
+  children: string;
 }
 
 const MDBlock: React.FC<Props> = ({ children }) => {
-
   return (
     <ReactMarkdown
       remarkPlugins={[remarkMath, remarkGfm]}
-      rehypePlugins={[rehypeKatex]}
+      rehypePlugins={[rehypeKatex, rehypeHighlight]}
     >
       {children}
     </ReactMarkdown>
   );
 };
 
-export default MDBlock
+export default MDBlock;
